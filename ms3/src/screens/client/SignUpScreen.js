c55d9cd6ad75667730db6bb00b91e6844eed56cd
Abdullah Hatem <abdullahhatem87@yahoo.com>
2023-05-27 16:05:37 +0300

    New SignUp Screen

--- ms3/src/screens/client/SignUpScreen.js
+++ ms3/src/screens/client/SignUpScreen.js
@@ -1,69 +1,207 @@
-import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import PersonalInfoStep from '../../components/client/RegisterForm/PersonalInfoStep';
-import PersonalDocsStep from '../../components/client/RegisterForm/PerosnalDocumets';
-import LinkBankAccountStep from '../../components/client/RegisterForm/LinkBankAccountStep';
-import TermsAndServicesStep from '../../components/client/RegisterForm/TermsAndServicesStep';
-import {
-  MDBBtn,
-  MDBContainer,
-  MDBCard,
-  MDBCardBody,
-  MDBInput,
-  MDBIcon,
-  MDBRow,
-  MDBCol,
-  MDBCheckbox
-}
-from 'mdb-react-ui-kit';
-import Navbar from '../../components/client/Navbar'
+import React, { useState, useEffect } from 'react';
+import "../../styles/signUp.css";
 import Footer from '../../components/client/Footer';
+import Navbar from '../../components/client/Navbar';
+import {Container} from 'react-bootstrap'
 
-const RegistrationForm = () => {
-  const [step, setStep] = React.useState(1);
 
-  const handleNextStep = () => {
-    setStep(step + 1);
+const SignUpScreen = () => {
+  const [activeForm, setActiveForm] = useState(1);
+
+  const handleNextForm = () => {
+    setActiveForm(activeForm + 1);
   };
 
-  const renderStep = () => {
-    switch (step) {
-      case 1:
-        return <PersonalInfoStep onNextStep={handleNextStep} />;
-      case 2:
-        return <PersonalDocsStep onNextStep={handleNextStep} />;
-      case 3:
-        return <LinkBankAccountStep onNextStep={handleNextStep} />;
-      case 4:
-        return <TermsAndServicesStep onNextStep={handleNextStep} />;
-      default:
-        return <PersonalInfoStep onNextStep={handleNextStep} />;
-    }
+  const handlePrevForm = () => {
+    setActiveForm(activeForm - 1);
   };
+
+  useEffect(() => {
+    const ioniconsModuleScript = document.createElement('script');
+    ioniconsModuleScript.src = 'https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js';
+    ioniconsModuleScript.type = 'module';
+    document.body.appendChild(ioniconsModuleScript);
+
+    const ioniconsScript = document.createElement('script');
+    ioniconsScript.src = 'https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js';
+    ioniconsScript.setAttribute('nomodule', '');
+    document.body.appendChild(ioniconsScript);
+
+    return () => {
+      document.body.removeChild(ioniconsModuleScript);
+      document.body.removeChild(ioniconsScript);
+    };
+  }, []);
 
   return (
-    <Container className='min-vh-100'>
-    <Navbar />
-    <MDBContainer className="mb-5 d-flex justify-content-center" style={{ maxWidth: "55%" }}>
-    <MDBRow className='m-auto'>
-      <MDBCol col='8'>
-      <MDBCard className='my-5 cascading-right bg-light' style={{  borderRadius: '10px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', height: '550px', width: '500px' }}>
-    <MDBCol md={{ span: 6, offset: 3 }} className="d-flex flex-column align-items-center py-5 ">
-      {renderStep()}
-
-      { (
-        <Button className='mt-4 btn-lg btn-primary' size='lg' onClick={handleNextStep}>{step !== 4 ? "Next": "Register"}</Button>
-      )}
-    </MDBCol>
-  </MDBCard>
-
-      </MDBCol>
-    </MDBRow>
-  </MDBContainer>
- <Footer />
-</Container>
-
+    <>
+      <Navbar />
+      <Container >
+      <div className="form__container my-4">
+        <div className="title__container">
+          <h1>Register</h1>
+          <p>Follow the 4 simple steps to create your profile</p>
+        </div>
+        <div className="body__container">
+          <div className="left__container">
+            <div className="side__titles">
+              <div className="title__name">
+                <h3>Your name</h3>
+                <p>Enter & press next</p>
+              </div>
+              <div className="title__name">
+                <h3>Desctibes</h3>
+                <p>select & press next</p>
+              </div>
+              <div className="title__name">
+                <h3>Services</h3>
+                <p>select & press next</p>
+              </div>
+              <div className="title__name">
+                <h3>Budget</h3>
+                <p>Select & press next</p>
+              </div>
+              <div className="title__name">
+                <h3>Complete</h3>
+                <p>Finally press submit</p>
+              </div>
+            </div>
+            
+            <div className="progress__bar__container">
+              <ul>
+                <li className={activeForm === 1 ? "active" : ""} id="icon1">
+                  <ion-icon name="person-outline"></ion-icon>
+                </li>
+                <li className={activeForm === 2 ? "active" : ""} id="icon2">
+                  <ion-icon name="book-outline"></ion-icon>
+                </li>
+                <li className={activeForm === 3 ? "active" : ""} id="icon3">
+                  <ion-icon name="layers-outline"></ion-icon>
+                </li>
+                <li className={activeForm === 4 ? "active" : ""} id="icon4">
+                  <ion-icon name="pricetag-outline"></ion-icon>
+                </li>
+                <li className={activeForm === 5 ? "active" : ""} id="icon5">
+                  <ion-icon name="mail-outline"></ion-icon>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right__container">
+            <fieldset id="form1" style={{ display: activeForm === 1 ? "block" : "none" }}>
+              <div className="sub__title__container">
+                <p>Step 1/5</p>
+                <h2>Let's start with your name</h2>
+                <p>Please fill the details below so that we can we can get in contact with you about our product</p>
+              </div>
+              <div className="input__container">
+                <label htmlFor="name">Enter your name</label>
+                <input type="text" />
+                <a className="nxt__btn" onClick={handleNextForm}>Next</a>
+              </div>
+            </fieldset>
+            <fieldset className={`active__form`} id="form2" style={{ display: activeForm === 2 ? "block" : "none" }}>
+              <div className="sub__title__container">
+                <p>Step 2/5</p>
+                <h2>What best describes you?</h2>
+                <p>Please let us know what type of business best describes you as an entrepreneur or businessman.</p>
+              </div>
+              <div className="input__container">
+                <div className="selection newB">
+                  <div className="imoji">
+                    <ion-icon name="happy"></ion-icon>
+                  </div>
+                  <div className="descriptionTitle">
+                    <h3>New Business</h3>
+                    <p>Started trading in the last 12 months</p>
+                  </div>
+                </div>
+                <div className="selection exitB">
+                  <div className="imoji">
+                    <ion-icon name="business"></ion-icon>
+                  </div>
+                  <div className="descriptionTitle">
+                    <h3>Existing Business</h3>
+                    <p>Have been operating beyond 12 months</p>
+                  </div>
+                </div>
+                <div className="buttons">
+                  <a className="prev__btn" onClick={handlePrevForm}>Back</a>
+                  <a className="nxt__btn" onClick={handleNextForm}>Next</a>
+                </div>
+              </div>
+            </fieldset>
+            <fieldset className={`active__form`} id="form3" style={{ display: activeForm === 3 ? "block" : "none" }}>
+              <div className="sub__title__container">
+                <p>Step 3/5</p>
+                <h2>What service are you looking for?</h2>
+                <p>Please let us know what type of business best describes you as an entrepreneur or businessman.</p>
+              </div>
+              <div className="input__container">
+                <div className="selection newB">
+                  <div className="imoji">
+                    <ion-icon name="desktop"></ion-icon>
+                  </div>
+                  <div className="descriptionTitle">
+                    <h3>Website Development</h3>
+                    <p>Development of online websites</p>
+                  </div>
+                </div>
+                <div className="selection exitB">
+                  <div className="imoji">
+                    <ion-icon name="phone-portrait"></ion-icon>
+                  </div>
+                  <div className="descriptionTitle">
+                    <h3>Development of Mobile App</h3>
+                    <p>Development of Android and iOS mobile apps</p>
+                  </div>
+                </div>
+                <div className="buttons">
+                  <a className="prev__btn" onClick={handlePrevForm}>Back</a>
+                  <a className="nxt__btn" onClick={handleNextForm}>Next</a>
+                </div>
+              </div>
+            </fieldset>
+            <fieldset className={`active__form`} id="form4" style={{ display: activeForm === 4 ? "block" : "none" }}>
+              <div className="sub__title__container">
+                <p>Step 4/5</p>
+                <h2>Please select your budget</h2>
+                <p>Please let us know the budget for your project so that we can give the right quote. Thanks!</p>
+              </div>
+              <div className="input__container">
+                <input type="range" min="10000" max="500000" value="250000" className="slider" />
+                <div className="output__value"></div>
+                <div className="buttons">
+                  <a className="prev__btn" onClick={handlePrevForm}>Back</a>
+                  <a className="nxt__btn" onClick={handleNextForm}>Next</a>
+                </div>
+              </div>
+            </fieldset>
+            <fieldset className={`active__form`} id="form5" style={{ display: activeForm === 5 ? "block" : "none" }}>
+              <div className="sub__title__container">
+                <p>Step 5/5</p>
+                <h2>Complete Submission</h2>
+                <p>Thanks for completing the form and for your time. Please enter your email below and submit the form.</p>
+              </div>
+              <div className="input__container">
+                <label htmlFor="Email">Enter your email</label>
+                <input type="text" />
+                <div className="buttons">
+                  <a className="prev__btn" onClick={handlePrevForm}>Back</a>
+                  <a className="nxt__btn" id="submitBtn" onClick={handleNextForm}>Next</a>
+                </div>
+              </div>
+            </fieldset>
+          </div>
+        </div>
+      </div>
+     
+      <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+      <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+      </Container>
+       <Footer/>
+    </>
   );
 };
 
-export default RegistrationForm;
+export default SignUpScreen;
---
+++ ms3/src/screens/client/TransferMoneyScreen.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { MDBInput } from 'mdbreact';
+import { FaDollarSign } from 'react-icons/fa'; // Import additional icon
+import Navbar from '../../components/client/Navbar'
+import Footer from '../../components/client/Footer';
+
+const BankTransferScreen = () => {
+  // Dummy data for available amount and current amount
+  const availableAmount = 5000;
+  const currentAmount = 10000;
+
+  return (
+    <>
+      <Navbar />
+      <Container className="min-vh-100">
+        <br />
+        <Row className="justify-content-center">
+          <Col md={6}>
+            <div className="text-center">
+              <h2 className="mb-4">Bank Transfer</h2>
+            </div>
+            <div className="card p-4 rounded shadow"> {/* Use a card component for styling */}
+              <Form>
+                <Form.Group controlId="formAccountNumber">
+                  <Form.Label>Account Number</Form.Label>
+                  <MDBInput type="text" outline />
+                </Form.Group>
+
+                <Form.Group controlId="formRecipientName">
+                  <Form.Label>Recipient Name</Form.Label>
+                  <MDBInput type="text" outline />
+                </Form.Group>
+
+                <Form.Group controlId="formAmount">
+                  <Form.Label>Amount</Form.Label>
+                  <div className="input-group"> {/* Add input group for icon */}
+                    <div className="input-group-prepend">
+                      <span className="input-group-text icon-container">
+                        <FaDollarSign />
+                      </span>
+                    </div>
+                    <MDBInput type="number" outline />
+                  </div>
+                </Form.Group>
+
+                <Form.Group controlId="formBank">
+                  <Form.Label>Bank</Form.Label>
+                  <MDBInput type="text" outline />
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Label>Available Amount</Form.Label>
+                  <div className="input-group"> {/* Display available amount */}
+                    <div className="input-group-prepend">
+                      <span className="input-group-text icon-container">
+                        <FaDollarSign />
+                      </span>
+                    </div>
+                    <MDBInput type="text" outline value={availableAmount} disabled />
+                  </div>
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Label>Current Amount</Form.Label>
+                  <div className="input-group"> {/* Display current amount */}
+                    <div className="input-group-prepend">
+                      <span className="input-group-text icon-container">
+                        <FaDollarSign />
+                      </span>
+                    </div>
+                    <MDBInput type="text" outline value={currentAmount} disabled />
+                  </div>
+                </Form.Group>
+
+                <div className="text-center">
+                  <Button variant="primary" className="px-4">
+                    Transfer
+                  </Button>
+                </div>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+       <Footer />
+    </>
+  );
+};
+
+export default BankTransferScreen;
